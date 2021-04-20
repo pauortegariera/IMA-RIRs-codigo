757c867cfcb1c18f945e694cd8dafbebed956425
Nicolas Pelella <nicolaspelella@gmail.com>
2021-04-19 23:28:46 -0300

    Modifique algunas boludeces, por ahi el plot no esta como vos lo habias hecho, eso podemos cambiarlo devuelta. El temita de que dividia por cero era porque estabamos multiplicando por t (este es el vector tiempo), en vez de T (este es un valor solo y es el tiempo de duracion del sweep, ejemplo: 5 seg)

--- Obteneción_RIRs.py
+++ Obteneción_RIRs.py
@@ -1,39 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import numpy as np
-import scipy.io.wavfile
+import scipy.io.wavfile 
 from matplotlib import pyplot as plt
 import soundfile as sf
 
+# lectura del .wav
 
-# LECTURA DE REGISTRO SONORO Y GENERACIÓN DE RIRs:
+audio,fs = sf.read("/Users/Miguel Pelella/.spyder-py3/prueba.wav") # Lectura del registro sonoro.
+t = np.arange(len(audio))/fs # Tiempo de duración del archivo.
+T = t[len(t)-1]
 
-audio,fs = sf.read("/Users/paulaortega/Desktop/TP IMA - RIRS/prueba.wav") # Lectura del registro sonoro.
-t= np.arange(len(audio))/fs # Tiempo de duración del archivo.
+# creación del sweep
 
-# creación del ssweep
+w1 = 2*np.pi*20; # Frecuencia angular inicial
+w2 = 2*np.pi*20000; # Frecuencia angular final
+K = (w1/np.log(w2/w1))*T; # Parámetros para la correcta generación del barrido.
+L = T/np.log(w2/w1);
+y = np.sin(K*((np.exp(t/L))-1)); #Barrido
 
-w1=2*np.pi*20; # Frecuencia angular inicial
-w2=2*np.pi*20000; # Frecuencia angular final
-K=(w1/np.log(w2/w1))*t; # Parámetros para la correcta generación del barrido.
-L=t/np.log(w2/w1);
-y=np.sin(K*((np.exp(t/L))-np.ones((1,len(t))))); #Barrido
+# modulacion
 
-w=(K/L)*np.exp(t/L); # Parámetros para la correcta generación del filtro inverso.
-m=w1/(2*np.pi*w);
-k=m*np.fliplr(y);
-k=k/max(abs(k));
+w = (K/L)*np.exp(t/L); # Parámetros para la correcta generación del filtro inverso.
+m = w1/(2*np.pi*w);
+
+# filtro invertido
+
+tInvertido = np.flip(t)
+yInvertido = np.sin(K*((np.exp(tInvertido/L))-1)); #Barrido
+k = m*yInvertido;
+k = k/max(abs(k));
+
+# obtencion de IR
+
+audioEspectro = np.fft.fft(audio)
+kEspectro = np.fft.fft(k)
+IR = audioEspectro*kEspectro
+
+"""
+En este caso el archivo de audio ya es una IR por eso da cualquier cosa el producto. 
+Pero si fuese la toma de un Sweep, daria bien. 
+"""
+
+# filtrado 
 
 
 
+# grafico
 
-
-fig,ax =plt.subplots()
-ax.plot(t,k)
-ax.set(xlabel='Tiempo(s)', ylabel='Amplitud')
+plt.plot(t,k)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Filtro Invertido')
 plt.show()
-
