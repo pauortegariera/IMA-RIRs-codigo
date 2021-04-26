9dbfb7929d66ff0c2fec098d4cb4e6bb35aba965
Nicolas Pelella <nicolaspelella@gmail.com>
2021-04-25 23:47:39 -0300

    Agregue ahi el proceso del filtrado

--- Obteneción_RIRs.py
+++ Obteneción_RIRs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.io.wavfile 
+from scipy import signal
 from matplotlib import pyplot as plt
 import soundfile as sf
 
@@ -40,9 +41,30 @@ En este caso el archivo de audio ya es una IR por eso da cualquier cosa el produ
 Pero si fuese la toma de un Sweep, daria bien. 
 """
 
-# filtrado 
+# filtrado 1/3
+
+nyquistRate = 44100
+factor = np.power(2,1.0/6.0)
+centerFrequency_Hz = np.array([39, 50, 63, 79, 99, 125, 157, 198, 250, 315, 
+397, 500, 630, 794, 1000, 1260, 1588, 2000, 2520, 3176, 4000, 5040, 6352, 8000, 10080, 
+12704, 16000])
+lowerCutoffFrequency_Hz=centerFrequency_Hz/factor;
+upperCutoffFrequency_Hz=centerFrequency_Hz*factor;
+filteredSpeech = []
+for lower,upper in zip(lowerCutoffFrequency_Hz, upperCutoffFrequency_Hz):
+    b, a = signal.butter( N=3, Wn=np.array([ lower, 
+    upper])/nyquistRate, btype='bandpass', analog=False, 
+    output='ba');
+    filteredSpeech.append(signal.filtfilt(b, a, audio))
 
 
+    
+"""
+filteredSpeechFFT= np.fft.fft(filteredSpeech)
+plt.plot(t,filteredSpeechFFT)
+plt.show()
+plt.plot(t,audioEspectro)
+plt.show()
 
 # grafico
 
@@ -51,3 +73,4 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Filtro Invertido')
 plt.show()
+"""
